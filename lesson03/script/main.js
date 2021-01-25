dd9f95af27f2c089dfa1af6ab634256c9995d02d
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-01-25 14:29:41 -0700

    week03 complete

---
+++ lesson03/script/main.js
@@ -0,0 +1,5 @@
+const options = {year:'numeric'};
+        document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', options);
+
+        let updatedate = document.lastModified;
+        document.getElementById('updatedate').textContent = updatedate;
