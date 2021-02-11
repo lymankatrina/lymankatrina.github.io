cc79011f4728b23c3dcc9340ac575dcb1bf96675
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-02-11 16:07:10 -0700

    finish 5 set up 6

--- lesson05/js/main.js
+++ lesson05/js/main.js
@@ -1,3 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+    }
 function initMap() {
     const myLatLng = { lat: -25.363, lng: 131.044 };
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -13,7 +16,7 @@ function initMap() {
 function myFunction() {
     var dayOfWeek = new Date().getDay();
     var banner = document.getElementById('pancakes');
-    if (dayOfWeek == 4) {
+    if (dayOfWeek == 5) {
         banner.style.display = 'block';
     } else {
         banner.style.display = 'none';
--- lesson06/js/main.js
+++ lesson06/js/main.js
@@ -1,3 +1,6 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+    }
 function initMap() {
     const myLatLng = { lat: -25.363, lng: 131.044 };
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -10,3 +13,13 @@ function initMap() {
         title: "Hello World!",
     });
 }
+function myFunction() {
+    var dayOfWeek = new Date().getDay();
+    var banner = document.getElementById('pancakes');
+    if (dayOfWeek == 5) {
+        banner.style.display = 'block';
+    } else {
+        banner.style.display = 'none';
+    }
+}
+myFunction();
