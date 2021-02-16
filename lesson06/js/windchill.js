cb6c1fc5e54c25d142b350e982959d2212e2ba57
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-02-16 07:48:08 -0700

    test js windchill

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,3 +1,19 @@
-f=35.74 + 0.6215t - 35.75s0.16 + 0.4275ts0.16
-t = air average temperature in fahrenheit
-s = windspeed in miles per HTMLSourceElement
+//    var t=document.getElementById("temperature").innerHTML;
+//    var ws=document.getElementById("windspeed").innerHTML;
+//    if(t<51 && ws>3.0){
+//        document.getElementById("windchill").innerHTML=Math.round((35.74+(0.6215*t)-(35.75*(math.pow(ws,0.16)))+((0.4275*t)*(math.pow(ws,0.16)))))*10)/10;
+//    }
+//     else{
+//        document.getElementById("windchill").innerHTML="N/A";
+//    }
+let t=33;
+let s=5;
+let a=0.6125*t;
+let ws=Math.pow(s,0.16);
+let f=35.74+a-(35.75*ws)+(0.4275*t*ws);
+if(t<51 && s>3){
+    document.getElementById("windchill").innerHTML=(Math.round(f*10)/10);
+}
+else{
+    document.getElementById("windchill").innerHTML="N/A";
+}
