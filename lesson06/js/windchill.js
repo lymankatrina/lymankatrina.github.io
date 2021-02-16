dc8d814efd1aa5deee654101f69806a73d91f6f3
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-02-16 08:10:38 -0700

    fix windchill

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -1,18 +1,8 @@
-//    var t=document.getElementById("temperature").innerHTML;
-//    var ws=document.getElementById("windspeed").innerHTML;
-//    if(t<51 && ws>3.0){
-//        document.getElementById("windchill").innerHTML=Math.round((35.74+(0.6215*t)-(35.75*(math.pow(ws,0.16)))+((0.4275*t)*(math.pow(ws,0.16)))))*10)/10;
-//    }
-//     else{
-//        document.getElementById("windchill").innerHTML="N/A";
-//    }
-let t=33;
-let s=5;
-let a=0.6125*t;
-let ws=Math.pow(s,0.16);
-let f=35.74+a-(35.75*ws)+(0.4275*t*ws);
+var t=document.getElementById("temperature").innerHTML;
+var s=document.getElementById("windspeed").innerHTML;
+var f=35.74+(0.6215*t)-(35.75*(Math.pow(s,0.16)))+((0.4275*t)*(Math.pow(s,0.16)));
 if(t<51 && s>3){
-    document.getElementById("windchill").innerHTML=(Math.round(f*10)/10);
+   document.getElementById("windchill").innerHTML=(Math.round(f*10)/10);
 }
 else{
     document.getElementById("windchill").innerHTML="N/A";
