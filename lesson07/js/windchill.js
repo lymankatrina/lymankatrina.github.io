6455dde808195938a5f77a879095b85ac7879d3a
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-02-22 18:08:01 -0700

    Finish week 7 gallery

---
+++ lesson07/js/windchill.js
@@ -0,0 +1,9 @@
+var t=document.getElementById("temperature").innerHTML;
+var s=document.getElementById("windspeed").innerHTML;
+var f=35.74+(0.6215*t)-(35.75*(Math.pow(s,0.16)))+((0.4275*t)*(Math.pow(s,0.16)));
+if(t<51 && s>3){
+   document.getElementById("windchill").innerHTML=(Math.round(f*10)/10);
+}
+else{
+    document.getElementById("windchill").innerHTML="N/A";
+}
