1971cb53ea8ba302b6a1f79eed2e8ccc4d1e989b
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-03-13 00:10:44 -0700

    api test

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=5d23be79eb6222459cb8f0bfb602b10b&units=imperial';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+//call the id in the html file to write to it//
+    const temperature = document.querySelector('#current-temp');
+    temperature.textContent = jsObject.main.temp;
+//get image//
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;// note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+});
