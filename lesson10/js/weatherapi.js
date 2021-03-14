c204a7d71502e0fcd8b7c524b1254d659636fe2a
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-03-13 23:57:39 -0700

    update api js file

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -27,15 +27,3 @@ document.getElementById('imagesrc').textContent = imagesrc;  // informational sp
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
 });
-
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-//get image//
-const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;// note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
-});
-
