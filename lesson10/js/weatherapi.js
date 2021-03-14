939e8ffc4029d231a6c4767f8080f581f9538f43
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-03-13 23:55:48 -0700

    weather api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -9,17 +9,33 @@ fetch(apiURL)
 
     const currently = document.querySelector('#currently');
     currently.textContent = jsObject.weather[0].description;
-
+    
     const humid = document.querySelector('#humidity');
     humid.textContent = jsObject.main.humidity;
 
     const windy = document.querySelector('#windspeed');
     windy.textContent = jsObject.wind.speed;
-
-//get image//
-    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;// note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
 });
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+//get image//
+const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;// note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+});
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+//get image//
+const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;// note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+});
+
