0bb22feecbace07895203211db51438bb145cd08
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-03-25 16:24:28 -0600

    final weather site

---
+++ lesson11/js/eventsFH.js
@@ -0,0 +1,32 @@
+//store the JSON resource
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+//use fetch to obtain a promise for
+fetch(requestURL)
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+    //select output location    
+    const cards = document.querySelector('.events');
+    const hometown = towns.filter(x => x.name == "Fish Haven");
+    hometown.forEach(hometown => {
+        let card = document.createElement('section');        
+        let article = document.createElement('article');
+        //let place = document.createElement('h1');
+        let event1 = document.createElement('p');
+        let event2 = document.createElement('p');
+        let event3 = document.createElement('p');
+        //use template literals        
+        //place.innerHTML = `${hometown.name}`;
+        event1.innerHTML = `${hometown.events[0]}`;
+        event2.innerHTML = `${hometown.events[1]}`;
+        event3.innerHTML = `${hometown.events[2]}`;
+        //article.append(place);
+        article.append(event1);
+        article.append(event2);
+        article.append(event3);
+        card.append(article);
+        cards.append(card);
+    });
+});
---
+++ lesson11/js/forecastSS.js
@@ -0,0 +1,32 @@
+let cityID = 'soda springs';
+let appid = '5d23be79eb6222459cb8f0bfb602b10b';
+const apiURL2 = `https://api.openweathermap.org/data/2.5/forecast?q=${cityID}&units=imperial&APPID=${appid}`;
+
+fetch(apiURL2)
+    .then((response) => response.json())
+    .then(function(jsObject2) {
+        const lists = jsObject2['list'];
+        //console.log(jsObject2);
+        let day = 0;
+        const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let item = 0;
+        //reduce the list array to the five forecasts....40
+        const str = jsObject2.list[item].dt_txt;
+        //filter
+        //console.log(n);
+        lists.forEach(forecast => {
+            if (forecast.dt_txt.includes('18:00:00')) {
+                let d = new Date(forecast.dt_txt);
+                //console.log(d);
+                //get forecast day
+                document.getElementById(`forecastday${day+1}`).textContent = dayofWeek[d.getDay()];
+                //get forecast icon
+                document.getElementById(`icon${day+1}`).setAttribute('src', `http://openweathermap.org/img/wn/${forecast.weather[item].icon}@2x.png`);
+                document.getElementById(`icon${day+1}`).setAttribute('alt', forecast.weather[item].description);
+                //get forecast temp
+                document.getElementById(`forecasttemp${day+1}`).textContent = forecast.main.temp+' F\u00B0';
+                //get forecast desc
+                document.getElementById(`forecastdesc${day+1}`).textContent = forecast.weather[item].description;
+                day++;
+        }});
+    });
