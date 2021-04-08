f0592224b77ea35e6a62be02cf7723b2e2b1dfbd
lymankatrina <77120233+lymankatrina@users.noreply.github.com>
2021-04-07 18:24:13 -0600

    home page

--- final/js/forecast.js
+++ final/js/forecast.js
@@ -1,10 +1,9 @@
-const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=37.6242&lon=-109.4782&appid=5d23be79eb6222459cb8f0bfb602b10b&units=imperial`;
+const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=37.6242&lon=-109.4782&exclude=minutely,hourly&appid=5d23be79eb6222459cb8f0bfb602b10b&units=imperial`;
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject);
-//call the id in the html file to write to it//
+
     const temperature = document.querySelector('#temp');
     temperature.textContent = jsObject.current.temp;
 
@@ -13,12 +12,6 @@ fetch(apiURL)
 
     const condition = document.querySelector('#cond');
     condition.textContent = jsObject.current.weather[0].description;
-
-    //const warning = document.querySelector('#alert');
-    //warning.textContent = jsObject.alerts.event;
-
-    //const alert = document.querySelector('#alertdesc');
-    //alert.textContent = jsObject.alerts.description;
 
     const forecastt1 = document.querySelector('#forecasttemp1');
     forecastt1.textContent = jsObject.daily[1].temp.day+ '\u00B0 F';
@@ -50,6 +43,38 @@ fetch(apiURL)
     forecasti3.setAttribute('src', `http://openweathermap.org/img/wn/${jsObject.daily[3].weather[0].icon}@2x.png`);
     forecasti3.setAttribute('alt', jsObject.daily[3].weather[0].description);         
 
+    const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let d = new Date();
+    let wd = d.getDay();
+
+    const forecastday1 = document.querySelector('#forecastday1');
+    forecastday1.textContent = dayofWeek[wd+1];
+
+    const forecastday2 = document.querySelector('#forecastday2');
+    forecastday2.textContent = dayofWeek[wd+2];
+
+    const forecastday3 = document.querySelector('#forecastday3');
+    forecastday3.textContent = dayofWeek[wd+3];
+    
+function myFunction() {
+    const warning = document.querySelector('#alert');
+    warning.textContent = 'Warning: '+jsObject.alerts.event;
+
+    const alert = document.querySelector('#alertdesc');
+    alert.textContent = 'Weather Advisory: '+jsObject.alerts.description;
+   
+    var banner = document.getElementById('#warnings');
+    if (warning = "") {
+        banner.style.display = 'none';
+    } else {
+        banner.style.display = 'block';
+    }
+}
+myFunction();
 });
+
+
+
+    
 
 
